74c86c5d882ca6ef56ed3e8c7e9f9bf5502bbe45
jungh150 <jungh150@naver.com>
2024-01-01 16:40:51 +0900

    기본적인 틀 생성
    폴더 생성, app.py, index.html, main.html 생성

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
+import sys
+application = Flask(__name__)
 
+# 메인
+@application.route("/")
+def hello():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', debug=True)
