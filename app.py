129086e4a93cef2c76315c3d30f81a9a9fdf1b0b
Shin <118626963+Shinwoo-Lee@users.noreply.github.com>
2024-01-03 14:27:18 +0900

    Update app.py
    - 네비게이션 바, 문제 등록, 문제 초기화
    - 초기화 버튼을 누르면 자동으로 문제 뽑기 페이지('/result')로 넘어가서 입력한 숫자에 대해 문제를 뽑아줌.
    - 유저 정보와 유저의 문제 정보에 대한 DB 생성 필요

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
-import sys
-application = Flask(__name__)
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
 
 # 메인
-@application.route("/")
-def hello():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    application.run(host='0.0.0.0', debug=True)
+# 문제 뽑기 페이지
+@app.route('/result', methods=['POST'])
+def result():
+    correct_numbers = request.form['correct_numbers']
+    correct_numbers_list = [int(num) for num in correct_numbers.split()]
+
+    wrong_numbers = request.form['wrong_numbers']
+    wrong_numbers_list = [int(num) for num in wrong_numbers.split()]
+
+    total_numbers_list = wrong_numbers_list + correct_numbers_list
+
+    if not correct_numbers_list:
+        return "맞은 문제 번호를 입력하세요."
+    
+    if not wrong_numbers_list:
+        return "틀린 문제 번호를 입력하세요."
+
+    selected_correct_number = random.choice(correct_numbers_list)
+    selected_wrong_number = random.choice(wrong_numbers_list)
+    selected_total_number = random.choice(total_numbers_list)
+    return render_template('result.html', selected_correctNumber=selected_correct_number, selected_wrongNumber=selected_wrong_number, selected_total_number=selected_total_number)
+
+if __name__ == '__main__':
+    app.run(debug=True)
